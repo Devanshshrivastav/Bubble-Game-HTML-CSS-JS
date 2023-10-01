1b7e6991fa71d14f9253be91471c1b3b0b1f8ae3
Devanshshrivastav <134669314+Devanshshrivastav@users.noreply.github.com>
2023-10-01 22:25:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+var timer = 60;
+var score = 0;
+var hitrn = 0;
+function increaseScore(){
+    score += 10;
+    document.querySelector("#scoreval").textContent = score;
+}
+
+function getNewHit(){
+    hitrn = Math.floor(Math.random() * 10);
+    // console.log(rn); 
+        document.querySelector("#hitval").textContent = hitrn;
+}
+
+function makeBubble() {
+    var clutter = "";
+
+    for (var i = 1; i <= 147; i++) {
+        rn = Math.floor(Math.random() * 10);
+        clutter += `<div class="bubble">${rn}</div>`;
+    }
+
+    document.querySelector("#pbtm").innerHTML = clutter;
+}
+
+function runTimer(){
+   var timerint = setInterval(function(){
+        if(timer>0){
+            timer--;
+            document.querySelector('#timerval').textContent = timer;
+        }
+        else{
+            clearInterval(timerint);
+            document.querySelector("#pbtm").innerHTML = `<h1>gamer over</h1> please try again`;
+        }
+    },1000);
+}
+  document.querySelector("#pbtm").addEventListener('click', function(dets){
+    var clickedNum = (Number(dets.target.textContent));
+    if (clickedNum === hitrn) {
+        increaseScore();
+        makeBubble();
+        getNewHit();
+    }
+  })
+
+
+runTimer();
+makeBubble();
+getNewHit();
